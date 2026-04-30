52a8e029eff9a5b61f2e5e4c7afb7cd1c8af13f0
Oswaldo <santoswaldos@gmail.com>
2026-04-30 00:05:50 -0600

    Se despliega con TomCat en el puerto 8080 la ruta /app/view donde se modifican los textos

---
+++ src/main/java/com/osantos/springboot/springmvc/app/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.osantos.springboot.springmvc.app.controllers;
+
+import com.osantos.springboot.springmvc.app.entities.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app") //Para agregar un prefijo es decir de primer nivel localhost:8080/app/view
+public class UserController {
+
+    //@GetMapping({"/view", "/", "/another"})
+    //@GetMapping({"/view", "/"}) -> Así se mapea para la raíz es decir localhost:8080 y en /view
+    @GetMapping("/view") //Aquí se asigna la ruta url. localhost:8080/view
+    public String viewData(Model model){
+        model.addAttribute("title","Hola mundo Spring Boot");
+        model.addAttribute("message","Esta es una aplicación de ejemplo usando Spring Boot");
+        model.addAttribute("user",new User("Oswaldo", "de los Santos"));
+        return "view"; // Aquí se debe poner el mismo nombre que se asignó al templete de html
+    }
+}
---
+++ src/main/java/com/osantos/springboot/springmvc/app/entities/User.java
@@ -0,0 +1,45 @@
+package com.osantos.springboot.springmvc.app.entities;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+    private String username;
+    private LocalDateTime createdAt;
+
+    public User() {
+    }
+
+    public User(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
