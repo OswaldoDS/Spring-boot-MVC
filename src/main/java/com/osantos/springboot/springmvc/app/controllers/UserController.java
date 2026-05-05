76d25e3f2b46352b91950a69415b3ad4a70e1c1f
Oswaldo <santoswaldos@gmail.com>
2026-05-04 22:07:04 -0600

    Validación de formulario, CSS con Bootstrap

--- src/main/java/com/osantos/springboot/springmvc/app/controllers/UserController.java
+++ src/main/java/com/osantos/springboot/springmvc/app/controllers/UserController.java
@@ -2,9 +2,12 @@ package com.osantos.springboot.springmvc.app.controllers;
 
 import com.osantos.springboot.springmvc.app.entities.User;
 import com.osantos.springboot.springmvc.app.services.IUserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
@@ -61,12 +64,19 @@ public class UserController {
 
     //Para cuando se recibe el objeto
     @PostMapping
-    public String form(User user, Model model, RedirectAttributes redirect) {
-        String message = (user.getId() != null &&  user.getId() > 0)?
-                "Usuario: " + user.getName() + " se ha actualizado con éxito!":
+    public String form(@Valid User user, BindingResult result, Model model, RedirectAttributes redirect, SessionStatus status) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("title", "Validando Formulario");
+            return "form";
+        }
+
+        String message = (user.getId() != null && user.getId() > 0) ?
+                "Usuario: " + user.getName() + " se ha actualizado con éxito!" :
                 "Usuario: " + user.getName() + " se ha creado con éxito!";
 
         service.save(user); // Puede ser un INSERT o un UPDATE
+        status.setComplete(); //Para borrar el objeto una vez que se usa
         redirect.addFlashAttribute("success", message);
         return "redirect:/users";
     }
--- src/main/java/com/osantos/springboot/springmvc/app/entities/User.java
+++ src/main/java/com/osantos/springboot/springmvc/app/entities/User.java
@@ -1,6 +1,9 @@
 package com.osantos.springboot.springmvc.app.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
@@ -11,10 +14,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty //Solo para String y válida que no sea vacío (Son reglas)
     private String name;
+    @NotEmpty
     private String lastname;
+    @NotEmpty
+    @Email //Valida que sea email
     private String email;
+    @NotEmpty
+    @Size(min = 4, max = 16)
     private String username;
+    @NotEmpty
     private String password;
 
     @Column(name = "created_at")
